3d370a678d1b1f10669df9a2b79472d17e927db6
kevingo <kevingo75@gmail.com>
2017-01-09 22:11:07 +0800

    add slice example

--- slice.go
+++ slice.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{1, 2, 3, 4, 5} // A slice type is a reference type, it has no specified length.
+	// A slice type is a reference type, it has no specified length.
+	slice := []int{1, 2, 3, 4, 5}
 	fmt.Println(slice)
 	fmt.Println("Length: ", len(slice))
 	fmt.Printf("Type: %T", slice)
@@ -18,12 +19,12 @@ func main() {
 	fmt.Println("slice2: ", slice2)
 	fmt.Println("len: ", len(slice2), " cap: ", cap(slice2))
 	fmt.Printf("address of slice2 %p\n", &slice2)
-	
-    slice2 = append(slice2, 0)
+
+	slice2 = append(slice2, 0)
 	fmt.Println("len: ", len(slice2), " cap: ", cap(slice2))
 	fmt.Printf("address of appended slice2 %p\n", &slice2)
-	
-    sub := slice2[2:4]
+
+	sub := slice2[2:4]
 	fmt.Println("sub : ", sub)
 	fmt.Println("sub len: ", len(sub), " cap: ", cap(sub))
 
@@ -36,9 +37,59 @@ func main() {
 	fmt.Println(slice3)
 	fmt.Printf("address of appended slice3 %p\n", &slice3)
 
-    // append two slices
-    println("\nExample3\n")
-    hello := []string{"Hello"}
-    world := []string{"world"}
-    fmt.Println(append(hello, world...)) // note you have to add ...
+	// append two slices
+	println("\nExample3\n")
+	hello := []string{"Hello"}
+	world := []string{"world"}
+	fmt.Println(append(hello, world...)) // note you have to add ...
+
+	println("\nExample4\n")
+	golang := []byte{'g', 'o', 'l', 'a', 'n', 'g'}
+	partial := golang[0:2] // pointer to the golang slice
+	fmt.Println(golang, partial)
+	golang[0] = 'a'
+	fmt.Println(golang, partial) // partial will also be changed to 'a'
+	iamarray := [3]int{1, 2, 3}
+	iamslice := iamarray[:] // a slice referencing the storage of iamarray
+	fmt.Println(iamarray, iamslice)
+	iamarray[0] = 0
+	fmt.Println(iamarray, iamslice) // iamslice also be changed
+
+	println("\nExample 5\n")
+	a := []int{1, 2, 3, 4, 5}
+	b := a[2:4]
+	fmt.Println(a, cap(a))
+	fmt.Println(b, cap(b))
+	b = b[:cap(b)]
+	fmt.Println(b, cap(b))
+
+	println("\nExample 6\n")
+	c := []int{1, 2, 3, 4, 5}
+	fmt.Println("before ", c, cap(c))
+	d := make([]int, len(c), (cap(c)+1)*2)
+	copy(d, c)
+	c = d
+	fmt.Println("after ", c, cap(c))
+
+	e := Filter(c, fn)
+	fmt.Println("my e slice ", e)
+}
+
+func fn(s int) bool {
+	if s%2 == 0 {
+		return true
+	}
+	return false
+}
+
+// Filter returns a new slice holding only
+// the elements of s that satisfy f()
+func Filter(s []int, fn func(int) bool) []int {
+	var p []int // == nil
+	for _, v := range s {
+		if fn(v) {
+			p = append(p, v)
+		}
+	}
+	return p
 }
