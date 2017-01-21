08d45f0837fe8c4e398720a158729917defa2c30
kevingo <kevingo75@gmail.com>
2017-01-21 11:24:50 +0800

    add new exemple

--- json.go
+++ json.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 )
 
 type Person struct {
-    First string
-    Last string
-    Age int
-    notExported int
+	First       string
+	Last        string
+	Age         int
+	notExported int
+}
+
+type MyUser struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 func main() {
-    p1 := Person{"Kevin", "Tsai", 20, 007}
-    fmt.Println("p1 struct: ", p1)
+	p1 := Person{"Kevin", "Tsai", 20, 007}
+	fmt.Println("p1 struct: ", p1)
 
-    j, _ := json.Marshal(p1)
-    fmt.Printf("Type %T\n", j)
-    fmt.Println(string(j))
+	// Example 1: Marchal return the json encode
+	j, _ := json.Marshal(p1)
+	fmt.Printf("Type %T\n", j)
+	fmt.Println(string(j))
+
+	// Example 2
+	json.NewEncoder(os.Stdout).Encode(&MyUser{1, "kevingo", time.Now()})
 }
